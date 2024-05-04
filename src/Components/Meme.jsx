526e14ad5f3990ccd13b31614683d2f99957f496
Ammarbashir <ammarbashirharuna2020@gmail.com>
2024-05-04 03:17:50 +0100

    generating meme functionality

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import MemesData from "./MemesData";
 const Meme = () => {
-    const [memeImage, setmemeImage ] = useState("")
+    const [meme, setmeme] = useState(({
+        topText: "",
+        BottomText: "",
+        randomImage: "https://i.imgflip.com/1g8my4.jpg"
+    }))
+    const [allMemeImages, setAllMemeImages] = useState(MemesData)
+  
     let url
     function Getmemes() {
-        const GetrandomMemes = MemesData.data.memes
+        const GetrandomMemes = allMemeImages.data.memes
         const randomArray = Math.floor(Math.random() * GetrandomMemes.length)
-        url = GetrandomMemes[randomArray].url
-        setmemeImage("GetrandomMemes")
+        const url = GetrandomMemes[randomArray].url
+        setmeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+      
       
     }
+   
     return ( 
     
         <main>
@@ -19,7 +30,7 @@ const Meme = () => {
                 <input type="text" className="form-input"placeholder="Bottom text" />
                <button className="form-button" onClick={Getmemes}>Generate New meme image</button>
             </div>
-            <img src={memeImage}  />
+            <img src={meme.randomImage}  className="mem-image"/>
             </main>
             
      );
